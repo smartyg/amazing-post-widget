826aaf162c0da852c10b0329b9c4ac7e5d7ad8c7
faugro <faugro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-20 10:44:23 +0000

    This is version 1.0.4

---
+++ amazing-post-widget/tags/1.0.4/amazing-posts-widget.php
@@ -0,0 +1,276 @@
+<?php
+/*
+Plugin Name: Amazing Posts Widget
+Plugin URI: http://pancenjoss.com
+Author: Faugro
+Author URI: http://pancenjoss.com
+Version: 1.0.4
+Description: Amazing way to show your post, you can easily set your column, row, option to show it as slideshow, also the best part is you also can choose custom post type.
+License: GPL2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/*  
+Copyright 2013  Fauzi Nugroho (email : fau.gro@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/ 
+
+// Block direct requests
+if( !defined('ABSPATH') )
+	die('-1');
+
+if( !defined('PJ_AP_Version') )
+	define( 'PJ_AP_Version', '1.0.0' );
+	
+define( 'PJ_AP_PATH', plugin_dir_path(__FILE__) );
+
+include PJ_AP_PATH.'aq_resizer.php';
+
+/** Initialize the widget on widgets_init */
+function pj_load_amazing_posts_widget() {
+	register_widget('PJ_Amazing_Posts_Widget');
+}
+add_action('widgets_init', 'pj_load_amazing_posts_widget' );
+
+/** Register styles & scripts */
+function register_apw_style() {
+	wp_register_style( 'amazing-pw-styles', plugins_url( 'css/amazing-pw.css', __FILE__ ) );
+	wp_enqueue_style( 'amazing-pw-styles' );
+}
+
+function register_apw_jquery() {
+	wp_enqueue_script ( 'jquery' );
+}
+
+function register_apw_script() {
+	wp_register_script( 'jquery-touchSwipe', plugins_url( 'js/jquery.touchSwipe.min.js', __FILE__ ), array('jquery'), '', true );
+	wp_register_script( 'jquery-ls', plugins_url( 'js/jquery.liquid-slider.min.js', __FILE__ ), array('jquery'), '', true );
+	wp_register_script( 'jquery_easing', plugins_url( 'js/jquery.easing.1.3.js', __FILE__ ), array('jquery'), '', true );
+}
+add_action( 'wp_enqueue_scripts', 'register_apw_style' );
+add_action( 'wp_enqueue_scripts', 'register_apw_jquery' );
+add_action( 'wp_enqueue_scripts', 'register_apw_script' );
+		
+global $pagenow;
+
+if ( defined("WP_ADMIN") && WP_ADMIN ) {
+	if ( 'widgets.php' == $pagenow || 'post.php' == $pagenow ) {
+		function register_apw_admin_style( $dir ) {
+			wp_register_style( 'amazing-posts-widget',  plugins_url( 'css/admin.css', __FILE__ ), array() );
+			wp_enqueue_style( 'amazing-posts-widget' );
+		}
+		add_action( 'admin_enqueue_scripts', 'register_apw_admin_style' );
+	}
+}
+
+
+/**
+ * Amazing Posts Widget Class
+ */
+class PJ_Amazing_Posts_Widget extends WP_Widget {
+	
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+		
+		parent::__construct(
+	 		'pj_ap_widget', // Base ID
+			'Amazing Posts Widget', // Name
+			array( 'description' => __( 'Display posts with Amazing way', 'pancenjoss_amazing' ) ) // Args
+		);
+		
+		// Hooks fired when the Widget is activated and deactivated
+		// register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		// register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+		
+		
+		// $this->directory	= plugins_url( '/', __FILE__ );
+		
+	}
+	
+
+   /**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+    function widget($args, $instance) {	
+        extract( $args );
+		extract( $instance );
+				
+		$title = apply_filters( 'widget_title', empty( $title ) ? '' : $title );
+		
+		// if ( empty($template) ) $template = 'amaz-columns.php';
+		
+		$post_types = get_post_types();
+		unset($post_types['page'], $post_types['attachment'], $post_types['revision'], $post_types['nav_menu_item']);
+		
+		if($post_type == 'all') {
+			$post_type_array = $post_types;
+		} else {
+			$post_type_array = $post_type;
+		}
+		
+		$number = $rows*$columns;
+
+		// Setup the query
+		$i=0;
+		$argss=array();
+		while($i<$pages_number)
+		{
+			$argss[$i] = array(
+				'ignore_sticky_posts' => 1,
+				'cat' 				=> $categories,
+				'post_type'			=> $post_type_array,
+				'post_status'		=> array('publish', 'inherit'),
+				'posts_per_page'	=> $number,
+				'offset'			=> $number*$i+$offset,
+				'orderby'			=> $orderby,
+				'order'				=> $order,
+			);
+			$i++;
+		}
+		
+		// Get the posts for this instance
+		
+		include ( PJ_AP_PATH . 'views/'.$template );
+        
+    }
+
+    /**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+    function update( $new_instance, $old_instance ) {
+		
+		// Get our defaults to test against
+		// $this->posttypes	= get_post_types( array('public' => true ), 'objects' );
+		// $this->taxonomies	= get_taxonomies( array('public' => true ), 'objects' );
+		// $this->thumbsizes	= get_intermediate_image_sizes();
+		$this->orderbys		= array(
+			'date'		 	=> __('Publish Date', 'pancenjoss_amazing'),
+			'title'			=> __('Title', 'pancenjoss_amazing'),
+			'menu_order'	=> __('Menu Order', 'pancenjoss_amazing'),
+			'ID'			=> __('Post ID', 'pancenjoss_amazing'),
+			'author'		=> __('Author', 'pancenjoss_amazing'),
+			'name'	 		=> __('Post Slug', 'pancenjoss_amazing'),
+			'comment_count'	=> __('Comment Count', 'pancenjoss_amazing'),
+			'rand'			=> __('Random', 'pancenjoss_amazing'),
+		);
+		$this->orders		= array(
+			'ASC'	=> __('Ascending', 'pancenjoss_amazing'),
+			'DESC'	=> __('Descending', 'pancenjoss_amazing'),
+		);
+		
+		$instance 				= $old_instance;
+		$instance['title']		= strip_tags( $new_instance['title'] );
+		$instance['categories']	= $new_instance['categories'];
+		$instance['post_type']	= $new_instance['post_type'];
+		$instance['offset']		= (int) $new_instance['offset'];
+		$instance['orderby']	= ( array_key_exists( $new_instance['orderby'], $this->orderbys ) ? $new_instance['orderby'] : 'date' );
+		$instance['order']		= ( array_key_exists( $new_instance['order'], $this->orders ) ? $new_instance['order'] : 'DESC' );
+		$instance['thumbnail']	= ( isset(  $new_instance['thumbnail'] ) ? (int) $new_instance['thumbnail'] : '0' );
+		$instance['post_title']	= ( isset(  $new_instance['post_title'] ) ? (int) $new_instance['post_title'] : '0' );
+		$instance['excerpt_length']		= (int) $new_instance['excerpt_length'];
+		$instance['pages_number']		= (int) $new_instance['pages_number'];
+		$instance['rows']		= (int) $new_instance['rows'];
+		$instance['columns']		= (int) $new_instance['columns'];
+		$instance['img_width']		= (int) $new_instance['img_width'];
+		$instance['img_height']		= (int) $new_instance['img_height'];
+		$instance['template']	= strip_tags( $new_instance['template'] );
+        
+        return $instance;
+      
+    }
+
+	function get_amaz_templates(){
+		$templates = array();
+
+		foreach(glob(plugin_dir_path(__FILE__).'/views/amaz*.php') as $file) {
+			$templates[] = basename($file);
+		}
+		foreach(glob(PJ_AP_PATH.'/views/amaz*.php') as $file){
+			$templates[] = basename($file);
+		}
+		$templates = array_unique($templates);
+		sort($templates);
+
+		return $templates;
+	}
+
+    /**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+    function form( $instance ) {
+    	
+   		// $this->posttypes	= get_post_types( array('public' => true ), 'objects' );
+		// $this->taxonomies	= get_taxonomies( array('public' => true ), 'objects' );
+		// $this->thumbsizes	= get_intermediate_image_sizes();
+		$this->orderbys		= array(
+			'date'		 	=> __('Publish Date', 'pancenjoss_amazing'),
+			'title'			=> __('Title', 'pancenjoss_amazing'),
+			'menu_order'	=> __('Menu Order', 'pancenjoss_amazing'),
+			'ID'			=> __('Post ID', 'pancenjoss_amazing'),
+			'author'		=> __('Author', 'pancenjoss_amazing'),
+			'name'	 		=> __('Post Slug', 'pancenjoss_amazing'),
+			'comment_count'	=> __('Comment Count', 'pancenjoss_amazing'),
+			'rand'			=> __('Random', 'pancenjoss_amazing'),
+		);
+		$this->orders		= array(
+			'ASC'	=> __('Ascending', 'pancenjoss_amazing'),
+			'DESC'	=> __('Descending', 'pancenjoss_amazing'),
+		);
+		
+		$instance = wp_parse_args( (array) $instance, array(
+			'title'		=> '',
+			'categories' => 'all',
+			'post_type' => 'post',
+			'offset'	=> '0',
+			'orderby'	=> 'date',
+			'order'		=> 'DESC',
+			'post_title' => '1',
+			'excerpt_length' => '40',
+			'pages_number' => '1',
+			'rows'		=> '1',
+			'columns'	=> '3',
+			'thumbnail' => '1',
+			'img_width' => '383',
+			'img_height' => '200',
+			'template'	=> 'amaz-columns.php',
+		) );
+		
+		extract( $instance );
+
+		$templates = $this->get_amaz_templates();
+		
+		include ( PJ_AP_PATH. 'views/admin-amazing.php' );
+        
+    }	
+
+} // class PJ_Amazing_Posts_Widget
---
+++ amazing-post-widget/tags/1.0.4/views/amaz-hover.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * carousel Posts Widget: Default widget template
+ */
+
+// Block direct requests
+if ( !defined('ABSPATH') )
+	die('-1');
+
+wp_enqueue_script( 'jquery-touchSwipe' );
+wp_enqueue_script( 'jquery-ls' );
+wp_enqueue_script( 'jquery_easing' );
+global $post;
+$widget_no = rand(1,999);
+echo $before_widget;
+
+if ( !empty($title) )
+	echo $before_title . $title . $after_title;
+?>
+	<div class="liquid-slider"  id="slider-id-<?php echo $widget_no; ?>">
+		<?php $i=0;
+		while($i<$pages_number)
+		{
+			$carousel_posts = new WP_Query( $argss[$i] );
+			if( $carousel_posts->have_posts() && $pages_number >= $i+1 ) : ?>
+			<div class="tp-carousel-posts portf-cols cols-<?php echo $columns; ?> row">
+				<h2 class="title hidden">Slide <?php echo $i; ?></h2>
+			<?php while ( $carousel_posts->have_posts() ) : $carousel_posts->the_post(); ?>
+				<div id="carou-post-<?php the_ID(); ?>" <?php post_class(); ?> style="width:<?php echo (100/$columns); ?>%;">
+					<div class="margin hover">
+						<a href="<?php echo the_permalink(); ?>">
+							<?php
+								if( $thumbnail == true ) {
+									$thumb = get_post_thumbnail_id();
+									$img_url = wp_get_attachment_url( $thumb, 'full'); //get img URL
+									$timage = aq_resize( $img_url, $img_width, $img_height, true ); //resize & crop img
+							?>
+								<img src="<?php echo $timage; ?>" alt="">
+							<?php } ?>
+							<span class="title"><span><?php the_title(); ?></span></span>
+						</a>
+					</div>
+				</div>
+			<?php endwhile; ?>
+			</div><!-- .tp-carousel-posts.two-cols -->
+		<?php endif; 
+		$i++;
+		} ?>
+	</div>
+	<?php // Be sure to reset any post_data before proceeding
+
+		wp_reset_postdata(); ?>
+<?php if ( $pages_number >= '2' ) : ?>
+<script type="text/javascript">
+jQuery(function() {
+  /* Here is the slider using default settings */
+  jQuery('#slider-id-<?php echo $widget_no; ?>').liquidSlider({
+		autoHeight:true,
+		autoHeightEaseDuration: 800,
+		slideEaseFunction: "easeInOutCubic",
+		slideEaseDuration: 1000,
+		// autoSlide:true,
+		// autoSlideStopWhenClicked: false,
+		// autoSlideInterval: 5000,
+		dynamicArrows: false,
+		mobileNavigation: false,
+		useCSSMaxWidth: 1200
+	  });
+	
+  jQuery('.tp-carousel-posts.portf-cols a:has(span)').hover(function() { 
+		jQuery('span.title', this).fadeToggle(600); 
+	});
+});
+</script>
+<?php endif; ?>
+<?php echo $after_widget; ?>
